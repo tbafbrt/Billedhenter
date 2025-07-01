5ad56f4ceb66440213fbac54709440f52fdcfd6f
tbafbrt <146091290+tbafbrt@users.noreply.github.com>
2025-07-01 10:18:35 +0200

    med markdown

--- TABilledhenter.py
+++ TABilledhenter.py
@@ -581,8 +581,12 @@ def main_application():
             # Display found images and suggestions in merged format
             if results['found'] or results['missing']:
                 st.header("✅ Vælg de billeder du vil hente ned")
-                st.text("Herunder kan du vælge de billeder du vil hente ned, du kan vælge både billeder der matcher direkte og forslag til alternativer for manglende billeder. I bunden finder du knapper til at vælge alle billeder eller alle bilelder der er direkte matches.")
-                
+                beskrivelse = ('''Herunder kan du vælge de billeder du vil hente ned.  
+                               Du kan vælge både billeder der matcher direkte og forslag til alternativer for manglende billeder. 
+                               I bunden finder du knapper til at vælge alle billeder eller alle billeder der er direkte matches.
+                               '''
+                st.markdown(beskrivelse)
+
                 all_images = []
                 global_image_counter = 0  # Add global counter for unique keys
                 
